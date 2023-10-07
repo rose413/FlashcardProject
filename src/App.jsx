305ea81880e26c0cf711122cf36153a537df905d
rose413 <irenechang949@gmail.com>
2023-10-06 23:02:52 -0700

    Implemented the user input features

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import Flashcard from './components/flashcard';
 const App = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false); // Add isFlipped state
+  const [userGuess, setUserGuess] = useState('');
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 
   const handleNextCard = () => {
     if (currentCardIndex < cards.length - 1) {
       setCurrentCardIndex((prevIndex) => prevIndex + 1);
       setIsFlipped(false);
+      setUserGuess('');
+      setIsAnswerCorrect(null);
     }
   };
 
@@ -18,6 +22,16 @@ const App = () => {
     if (currentCardIndex > 0) {
       setCurrentCardIndex((prevIndex) => prevIndex - 1);
       setIsFlipped(false);
+      setUserGuess('');
+      setIsAnswerCorrect(null);
+    }
+  };
+
+  const handleGuessSubmit = () => {
+    if (userGuess.trim().toLowerCase() === cards[currentCardIndex].back.toLowerCase()) {
+      setIsAnswerCorrect(true);
+    } else {
+      setIsAnswerCorrect(false);
     }
   };
 
@@ -32,6 +46,21 @@ const App = () => {
         isFlipped={isFlipped} 
         setIsFlipped={setIsFlipped} 
       />
+      <div className="guess-container">
+        {!isFlipped && (
+          <>
+            <input
+              type="text"
+              placeholder="Enter your guess"
+              value={userGuess}
+              onChange={(e) => setUserGuess(e.target.value)}
+            />
+            <button onClick={handleGuessSubmit}>Submit</button>
+          </>
+        )}
+        {isAnswerCorrect === true && <p className="correct">Correct!</p>}
+        {isAnswerCorrect === false && <p className="incorrect">Incorrect!</p>}
+      </div>
       <button onClick={handlePreviousCard} disabled={currentCardIndex === 0}>Back</button>
       <button onClick={handleNextCard}>Next</button>
     </div>
